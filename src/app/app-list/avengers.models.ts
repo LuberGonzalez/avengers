280ff9dc8adbcf005feb022a9cf39171b31294ad
LuberGonzalez <luber.gonzalez.77@gmail.com>
2022-05-10 20:44:02 +0200

    Enviando la app a Git Hub

---
+++ src/app/app-list/avengers.models.ts
@@ -0,0 +1,7 @@
+export interface Avenger {
+  hero: String;
+  name: String;
+  link: String;
+  img: String;
+  isActive: boolean;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppListComponent } from './app-list/app-list.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppListComponent,
+    AppFooterComponent,
   ],
   imports: [
     BrowserModule
